76794b943165d0beafa0b1c2936ac7c420499489
nelsonchu0719 <nelson.cf.chu@gmail.com>
2016-03-21 21:23:23 -0400

    .

--- src/main/java/workload/RandomWorkloadGenerator.java
+++ src/main/java/workload/RandomWorkloadGenerator.java
@@ -21,8 +21,8 @@ public class RandomWorkloadGenerator {
 
         final int reduceSpeedMultiplier = 20;
         final int totalFileNumber = 40;
-        final int fileSizeMin = 300; //MB
-        final int fileSizeMax = 500; //MB
+        final int fileSizeMin = 75; //MB
+        final int fileSizeMax = 150; //MB
 
         final int totalReadOperation = 500;
 
